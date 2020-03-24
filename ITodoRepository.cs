7af9339a147395de0491e872dc773ea652a21e36
Adexandria <40776751+Adexandria@users.noreply.github.com>
2020-03-24 21:24:57 +0100

    Update ITodoRepository.cs

--- ITodoRepository.cs
+++ ITodoRepository.cs
@@ -8,15 +8,19 @@ using Tasks.Model;
 namespace Tasks.Data
 {
    public interface ITodoRepository 
-    {
+   { 
+      //To add
         void Add<T>(T entity) where T : class;
+      //to delete
         void Delete<T>(T entity) where T : class;
+      //for the Todolist
         Task<Todo[]> GetTodoModels();
         Task<Todo> GetTodoModel(int name);
         Task<Todo> GetTodoModelName(string name);
-
+       Task<Todo[]> GetTodoModelsByDate(DateTime date);
+      //to save changes
         Task<bool> SaveChanges();
-        Task<Todo[]> GetTodoModelsByDate(DateTime date);
+        
         
     }
 }
